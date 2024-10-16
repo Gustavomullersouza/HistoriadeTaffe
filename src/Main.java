741ef2db5341bc168faacfe43c3317dd663094a8
marcolapcx <168039947+marcolapcx@users.noreply.github.com>
2024-10-16 19:46:20 -0300

    taffe

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class Main {
     public static void main(String[] args) {
 
+        Taffe taffe = new Taffe();
+        String fala = taffe.dizerOi();
+        System.out.println(fala);
+
+        taffe.falar("Gremio melhor que inter");
+        taffe.falar("Aprendendo Classes em Java");
+
+
+        taffe.andar(10);
 
     }
 }
--- src/Taffe.java
+++ src/Taffe.java
@@ -8,6 +8,17 @@ public class Taffe {
         return "Oi pessoal";
     }
 
+    public void falar(String oqueFalar){
+        System.out.println(oqueFalar);
+    }
+
+    public void andar (int quantidadePassos){
+        for (int i = 0; i <= quantidadePassos; i++){
+            System.out.println("Passo: " + i);
+
+        }
+    }
+
     //andar
     //falar
     //dizer oi
