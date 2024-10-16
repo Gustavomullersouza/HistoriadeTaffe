24551a038463857f758ea968f82a2fd57ae8f972
marcolapcx <168039947+marcolapcx@users.noreply.github.com>
2024-10-16 19:26:32 -0300

    taffe

---
+++ src/Taffe.java
@@ -0,0 +1,19 @@
+public class Taffe {
+
+    public int numeracao;
+
+    public String versao;
+
+    public String dizerOi(){
+        return "Oi pessoal";
+    }
+
+    //andar
+    //falar
+    //dizer oi
+    //mandar mensagem bravo
+    //limpar casa
+    //cantar uma musica
+    //mudar idioma]
+
+}
